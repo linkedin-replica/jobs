bcd83a3cfe4731b02062975514fbc52941a875aa
loay elzobaidy <loayelzobeidy@gmail.com>
2018-03-26 22:58:55 +0200

    JobsHandler modified

--- src/main/java/com/linkedin/replica/jobs/cache/handlers/impl/JobsHandler.java
+++ src/main/java/com/linkedin/replica/jobs/cache/handlers/impl/JobsHandler.java
@@ -1,6 +1,12 @@
 package com.linkedin.replica.jobs.cache.handlers.impl;
 import com.linkedin.replica.jobs.cache.handlers.CacheHandler;
+import com.linkedin.replica.jobs.models.Job;
+
+import java.io.IOException;
+import java.util.List;
 
 public interface JobsHandler extends CacheHandler{
+    public void saveJobListings(String userId, Object jobs) throws IOException;
+    List<Job> getJobListingsFromCache(String userId) throws IOException;
 
 }
